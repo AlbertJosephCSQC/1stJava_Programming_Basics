62cb887eea4c0e16b5064447f045cfed0b6089a2
jason <jasonithiel125@gmail.com>
2026-05-24 17:59:27 +0530

    fixed errors

---
+++ 3- Flow Control/Conditionals/Check Divisible/src/CheckDivisible.java
@@ -0,0 +1,18 @@
+public class CheckDivisible {
+    public static void main(String[] args) {
+        int x = 100;
+
+        /* Set the appropriate condition. */
+      /* Write the desired message. */
+        if (x % 10 == 0)
+        {
+            System.out.println("Number is divisible by 10");
+        }
+
+
+        else
+        {
+            System.out.println("Number is NOT divisible by 10");
+        }
+    }
+}
---
+++ 3- Flow Control/Conditionals/Positive Or Negative/src/PositiveOrNegative.java
@@ -0,0 +1,17 @@
+public class PositiveOrNegative {
+    public static void main(String[] args)
+    {
+        int x = 100;
+
+        /* Write your answer here */
+
+        if(x > 0)
+        {
+            System.out.println("x is positive");
+        }
+        else
+        {
+            System.out.println("x is negative");
+        }
+    }
+}
