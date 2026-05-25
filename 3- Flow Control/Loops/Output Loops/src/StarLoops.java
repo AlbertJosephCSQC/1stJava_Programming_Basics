0832da303cdbd72673423e9a5feb3b51e8d00b72
jason <jasonithiel125@gmail.com>
2026-05-25 09:44:38 +0530

    finished course.

--- 3- Flow Control/Loops/Output Loops/src/StarLoops.java
+++ 3- Flow Control/Loops/Output Loops/src/StarLoops.java
@@ -1,7 +1,16 @@
 public class StarLoops {
    public static void main(String[] args) {
 
-         int i,j;
-         /* Writ your answer here */
+       int i,j;
+       /* Writ your answer here */
+
+       for (i = 7; i >= 1; i--)
+       {
+           for (j = 0; j < i; j++)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+            }
       }
    }
